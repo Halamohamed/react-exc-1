9a5228105e7c50bbba846698dd0633588cccf26c
Hala Mohammed <halaalimohammed@gmail.com>
2020-09-25 12:15:57 +0200

    dice spel

---
+++ my-app/src/dice.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./dice.css";
+
+export default function Dice(props){
+    if( props.dice.locked ){
+        return(
+            <div className="dice locked" onClick={ () => props.klick()}>
+                {props.dice.value}
+            </div>
+        )
+    } else{
+        return(
+            <div className="dice " onClick={ () => props.klick()}>
+                {props.dice.value}
+            </div>
+        )
+    }
+    
+}
---
+++ my-app/src/dicebox.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import Dice from "./dice";
+import "./dicebox.css";
+
+export default class Dicebox extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            die: [
+                {
+                    value: null,
+                    locked: false
+                },
+                {
+                    value: null,
+                    locked: false
+                },
+                {
+                    value: null,
+                    locked: false
+                },
+                {
+                    value: null,
+                    locked: false
+                },
+                {
+                    value: null,
+                    locked: false
+                },
+            ],
+            totalSum: 0,
+            combinations: [
+                {
+                    pair: false,
+                    triss: false
+                }
+            ]
+        };
+    }
+    
+    componentDidMount(){
+        this.roll();
+    }
+
+    isPair(){
+        for(let i=1; i<=6; i++){
+            let counter =0;
+            this.state.die.forEach((dice) => {
+                if(dice.value == i){
+                    counter++;
+                }
+               
+            });
+            if(counter >=2){
+                let combinations = this.state.combinations;
+                combinations.pair = true;
+                this.setState( {
+                    combinations :combinations,
+                });
+            }
+            
+        }
+        
+    }
+
+    roll(){
+        let combinations = this.state.combinations;
+        combinations.pair = false;
+        this.setState({
+            combinations : combinations,
+        });
+
+        let total = 0
+        // Trestegsmetod för att ändra i en array i state 
+        let tempDice = this.state.die
+        tempDice.forEach((dice) => {
+            if(!dice.locked){
+               dice.value = Math.ceil(Math.random()* 6);
+            }
+            total += dice.value;
+        });
+        this.setState({
+            die: tempDice,
+            totalSum: total,
+        });
+        this.isPair();
+
+    }
+
+    lock(index){
+        let tempDice = this.state.die
+        tempDice[index].locked = !tempDice[index].locked;
+        
+        this.setState({
+            die: tempDice,
+        });
+    }
+
+    render(){
+        return <div>
+            <h1>Dicebox</h1>
+            <p>Totalsumma : {this.state.totalSum }</p>
+            <p>{this.state.combinations.pair ? "par": "Ej Par"}</p>
+            <section className="dicebox">
+            {this.state.die.map((dice, index) => {
+                <Dice 
+                key= {index}
+                dice= {dice}
+                klick= {() => this.lock(index)} />
+            })}
+            </section>
+            <button onClick={() => this.roll()}>Rulla </button>
+        </div>
+    }
+}
